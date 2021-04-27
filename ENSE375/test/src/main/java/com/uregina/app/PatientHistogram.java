9c2369a861d592c5af49df7d623138089db60b40
anuprasb6541 <55845790+anuprasb6541@users.noreply.github.com>
2021-04-27 00:18:25 -0600

    Create PatientHistogram.java

---
+++ ENSE375/test/src/main/java/com/uregina/app/PatientHistogram.java
@@ -0,0 +1,52 @@
+package com.uregina.app;
+// import com.uregina.exception.*;
+// import java.util.ArrayList;
+
+public class PatientHistogram 
+{
+
+	final int Y_MAX = 20;
+	final int X_MAX = 10;
+
+	private int[][] patientCount;
+    	public PatientHistogram() 
+    	{
+    		patientCount = new int[X_MAX][Y_MAX];
+    	}
+    	/**
+	*
+	* @return boolean: true if the a patient can be added from that region 
+	*/
+    	public boolean addAPatientToRegion(int VIndex,int HIndex)
+    	{
+    		if( VIndex < Y_MAX && VIndex >= 0 &&
+				HIndex < X_MAX && HIndex >= 0){
+					patientCount[HIndex][VIndex]++;
+					return true;
+				}
+    		return false;	
+    	}
+	/**
+	*
+	* @return boolean: true if the a patient can be removed from that region 
+	*/
+    	public boolean deleteAPatientFromRegion(int VIndex,int HIndex)
+    	{
+    		if( VIndex < Y_MAX && VIndex >= 0 &&
+				HIndex < X_MAX && HIndex >= 0 && 
+				patientCount[HIndex][VIndex] > 0){
+					patientCount[HIndex][VIndex]--;
+					return true;
+				}
+    		
+    		return false;
+    	}
+    	public int getPatientsCountInRegion(int VIndex,int HIndex) throws IndexOutOfBoundsException
+    	{
+			if( !(VIndex < Y_MAX && VIndex >= 0 &&
+				HIndex < X_MAX && HIndex >= 0)){
+					throw new IndexOutOfBoundsException();
+				}
+    		return patientCount[HIndex][VIndex];	
+    	}
+}
