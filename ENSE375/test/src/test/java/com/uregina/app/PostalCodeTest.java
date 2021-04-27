dabb47882aaaf33773e0be0492602df374877378
anuprasb6541 <55845790+anuprasb6541@users.noreply.github.com>
2021-04-27 00:24:06 -0600

    Create PostalCodeTest.java

---
+++ ENSE375/test/src/test/java/com/uregina/app/PostalCodeTest.java
@@ -0,0 +1,99 @@
+package com.uregina.app;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+//Implemented by Tristan Brown-Hannibal
+//200400651
+/**
+ * Unit test for simple PostalCode.
+ */
+public class PostalCodeTest 
+{
+ 
+ 
+
+    @Test
+    public void isValidPostCode_Valid_True(){
+       
+        assertTrue(PostalCode.isValidPostalCode("K1T-5X2"));
+    }
+
+
+    @Test
+    public void isCorrectVerticalIndex_Correct_True(){
+        PostalCode pc = null;
+        try{
+            pc = new PostalCode("K1B-5X2");
+
+        }catch(Exception e){
+
+        }
+        assertEquals(1,pc.getRegionVerticalIndex());
+    }
+
+    @Test
+    public void isCorrectHorizontalIndex_Correct_True(){
+        PostalCode pc = null;
+        try{
+            pc = new PostalCode("K1B-5X2");
+
+        }catch(Exception e){
+
+        }
+        assertEquals(5,pc.getRegionHorizontalIndex());
+    }
+
+    @Test
+    public void isCorrectPostalCode_Correct_True(){
+        PostalCode pc = null;
+        try{
+            pc = new PostalCode("K1B-5X2");
+
+        }catch(Exception e){
+
+        }
+        assertEquals("K1B-5X2",pc.getPostalCode());
+    }
+
+    @Test
+    public void makePostCode_True(){
+       try{
+        PostalCode pc = new PostalCode("K1T-5X2");
+       }catch(Exception e){
+
+       }
+    }
+
+    @Test
+    public void makePostCode_False(){
+       try{
+        PostalCode pc = new PostalCode("K1T X2");
+       }catch(Exception e){
+       }
+    }
+    @Test
+    public void isValidPostCode_NonValidLastLetter_False(){
+       
+        assertFalse(PostalCode.isValidPostalCode("K1T-5XF"));
+    }
+
+    @Test
+    public void isValidPostCode_NonValidSpace_False(){
+       
+        assertFalse(PostalCode.isValidPostalCode("K1T 5X2"));
+    }
+
+    @Test
+    public void isValidPostCode_NonValidLowerCase_False(){
+       
+        assertFalse(PostalCode.isValidPostalCode("K1t-5xf"));
+    }
+
+
+    @Test
+    public void isValidPostCode_WrongPrefix_False(){
+       
+        assertFalse(PostalCode.isValidPostalCode("T2t-5xf"));
+    }
+}
